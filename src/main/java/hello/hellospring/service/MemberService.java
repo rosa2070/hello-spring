758a22652d7d75adb0a7d3abb2a06311980c9a6b
jaeeunhyun <rosa2070@naver.com>
2021-03-28 12:06:17 +0900

    ghkjhj

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,17 +24,10 @@ public class MemberService {
      */
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
-
-        try {
             validateDuplicateMember(member); //중복 회원 검증
             memberRepository.save(member);
             return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+
     }
 
     private void validateDuplicateMember(Member member) {
@@ -49,14 +42,9 @@ public class MemberService {
      */
 
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
+
             return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+
     }
 
     public Optional<Member> findOne(Long memberId) {
